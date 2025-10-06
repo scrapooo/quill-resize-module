e3841d57c3557da4d10dc0d671ed3df5567b134d
王超 <cwcchunter@gmail.com>
2025-10-06 22:04:11 +0800

    fix: fixed the issue with incorrect behavior of the size restore button.

--- src/ResizePlugin.ts
+++ src/ResizePlugin.ts
@@ -28,7 +28,7 @@ const template = `
     <a class="btn" data-type="width" data-styles="width:100%">100%</a>
     <a class="btn" data-type="width" data-styles="width:50%">50%</a>
     <span class="input-wrapper"><input data-type="width" maxlength="3" /><span class="suffix">%</span><span class="tooltip">{5}</span></span>
-    <a class="btn" data-type="width" data-styles="width:auto">{4}</a>
+    <a class="btn" data-type="width" data-styles="width:auto; height:auto;">{4}</a>
   </div>
   <div class="group">
     <a class="btn" data-type="align" data-styles="float:left">{1}</a>
